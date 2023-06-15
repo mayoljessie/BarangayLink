0ea77a3896799d531fca06442d40f5e053084e53
mayoljessie <98403781+mayoljessie@users.noreply.github.com>
2023-06-15 19:37:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+function openNews() {
+    var overlay = document.getElementById("newsOverlay");
+    overlay.style.display = "flex";
+}
+
+function closeNews() {
+    var overlay = document.getElementById("newsOverlay");
+    overlay.style.display = "none";
+  }
+
+function openEvent() {
+    var overlay = document.getElementById("eventOverlay");
+    overlay.style.display = "flex";
+}
+function closeEvent() {
+    var overlay = document.getElementById("eventOverlay");
+    overlay.style.display = "none";
+  }
+
+function openAdvisory() {
+    var overlay = document.getElementById("advisoryOverlay");
+    overlay.style.display = "flex";
+}
+function closeAdvisory() {
+    var overlay = document.getElementById("advisoryOverlay");
+    overlay.style.display = "none";
+  }
+
+function openUpdates() {
+    var overlay = document.getElementById("UpdatesOverlay");
+    overlay.style.display = "flex";
+}
+function closeUpdates() {
+    var overlay = document.getElementById("UpdatesOverlay");
+    overlay.style.display = "none";
+  }
+
+function openRequest() {
+    var overlay = document.getElementById("RequestOverlay");
+    overlay.style.display = "flex";
+}
+function closeRequest() {
+    var overlay = document.getElementById("RequestOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openAppointment() {
+    var overlay = document.getElementById("AppointmentOverlay");
+    overlay.style.display = "flex";
+}
+function closeAppointment() {
+    var overlay = document.getElementById("AppointmentOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openMessage() {
+    var overlay = document.getElementById("MessageOverlay");
+    overlay.style.display = "flex";
+}
+function closeMessage() {
+    var overlay = document.getElementById("MessageOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openAnnouncement() {
+    var overlay = document.getElementById("AnnouncementOverlay");
+    overlay.style.display = "flex";
+}
+function closeAnnouncement() {
+    var overlay = document.getElementById("AnnouncementOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openForums() {
+    var overlay = document.getElementById("ForumsOverlay");
+    overlay.style.display = "flex";
+}
+function closeForums() {
+    var overlay = document.getElementById("ForumsOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openDiscussions() {
+    var overlay = document.getElementById("DiscussionsOverlay");
+    overlay.style.display = "flex";
+}
+function closeDiscussions() {
+    var overlay = document.getElementById("DiscussionsOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openFeedbacks() {
+    var overlay = document.getElementById("FeedbacksOverlay");
+    overlay.style.display = "flex";
+}
+function closeFeedbacks() {
+    var overlay = document.getElementById("FeedbacksOverlay");
+    overlay.style.display = "none";
+  }
